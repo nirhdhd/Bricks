67a174f54e8985fc32576cf926fb35246724840d
nir.hada <nir.hada@alljobs.co.il>
2020-07-05 17:07:54 +0300

    update

--- src/components/createWorksheet/step_two/step_two.jsx
+++ src/components/createWorksheet/step_two/step_two.jsx
@@ -3,23 +3,18 @@ import "../step_two/step_two.scss";
 import $ from "jquery"
 
 function Step_two(props) {
-  const [wordsStore, setWordsStore] = useState([]);
+  //const [wordsStore, setWordsStore] = useState([]);
+  //const [wordsStoreToRender, setWordsStoreToRender] = useState();
+  let wordsStore = []
   let key = 0;
-  const [inptVal, setInptVal] = useState("");
-
+  let wordsStoreToRender;
   useEffect(() => {
     $(".sp").click(() => {
       $(this).find("span").css("color", "#ffc38b")
     })
     fromInputToSpan(props.textVal);
-
   });
 
-  // $("span").click(function (e) {
-  //   console.log(e.target.id)
-  //   console.log(e.target.innerHTML)
-  //   setWordsStore([...wordsStore, { k: e.target.id, v: e.target.innerHTML }])
-  // });
 
   // const handleSpanClick = (word, key) => {
   //   setWordsStore([...wordsStore, { k: key, v: word }])
@@ -27,19 +22,35 @@ function Step_two(props) {
 
 
   const fromInputToSpan = (inptVal) => {
+
     let words = inptVal.split(' ');
     words.map(word => {
+
       let word_span = document.createElement("span");   // Create a <button> element
       word_span.innerHTML = word;
       word_span.id = key;
       word_span.className = "word";
       word_span.className = "sp";
-      // word_span.addEventListener("click", function (e) {
-      //   console.log(e.target.id, e.target.innerHTML)
-      //   setWordsStore([...wordsStore, { k: e.target.id, v: e.target.innerHTML }])
-      //   console.log("wordsStore", wordsStore);
-      // });
+
+      word_span.addEventListener("click", function (e) {
+
+        $("#" + e.target.id).css("color", "white");
+
+        let wordToInsert = {
+          key: e.target.id,
+          value: e.target.innerHTML
+        }
+
+        wordsStore = [...wordsStore, wordToInsert];
+        wordsStoreToRender = wordsStore.map(x => <div id={x.key}> {x.value} </div>);
+        // setWordsStoreToRender(wordsStore.map(x => <div id={x.key}> {x.value} </div>));
+        console.log("wordsStore", wordsStore);
+        //console.log(e.target.id, e.target.innerHTML)
+        // setWordsStore([...wordsStore, { k: e.target.id, v: e.target.innerHTML }])
+        //console.log(wordsStore);
+      });
       $("#inputPlace").append(word_span);
+
       key++;
     })
   }
@@ -58,6 +69,7 @@ function Step_two(props) {
           style={{ flex: 1, backgroundColor: "#fff3cd", textAlign: 'center', borderRadius: 10 }}
         >
           <h3 style={{ marginTop: 0 }}  >words store</h3>
+          {wordsStoreToRender}
         </div>
         <div style={{ flex: 0.1 }}></div>
       </div>
